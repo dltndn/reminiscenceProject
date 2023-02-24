718a0281e1e81b8852ab35516bf2ca17296267f4
dltndn <james98099@gmail.com>
2023-02-24 23:20:08 +0900

    feat: html템플릿 작성

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const bodyParser = require('body-parser');
+const testJs = require('./test')
 
 const port = 3200;
 
@@ -14,11 +15,19 @@ app.get("/b", function (req, res, next) {
   res.send("Hello b!");
     // next();
   },
-  function (req, res) {
-    console.log("Hello from B!");
-    res.redirect("/");
-  }
+  // function (req, res) {
+  //   console.log("Hello from B!");
+  //   res.redirect("/");
+  // }
 );
+
+app.get("/calendar", function (req, res) {
+  res.send(testJs.calendarHtml)
+})
+
+app.get("/hi", function (req, res) {
+  res.send(testJs.hi)
+})
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}`);
---
+++ test.js
@@ -0,0 +1,28 @@
+module.exports = {
+    calendarHtml : `<!DOCTYPE html>
+    <html>
+    <head>
+      <title>Calendar</title>
+      <style>
+        table {
+          border-collapse: collapse;
+          width: 100%;
+        }
+        td, th {
+          border: 1px solid black;
+          padding: 8px;
+          text-align: center;
+        }
+      </style>
+    </head>
+    <body>
+      <h1>Calendar</h1>
+     <input type="datetime">
+      <script>
+        alert("hi")
+      </script>
+    </body>
+    </html>
+    `,
+    hi : `hi`
+}
